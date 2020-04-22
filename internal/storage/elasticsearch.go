646494003c6779f8c5bb3a6811205023e4222e50
Wesley Ramos <wesley-ramos-de-paula@hotmail.com>
2020-04-22 17:18:29 -0300

    Removing unnecessary spaces

--- internal/storage/elasticsearch.go
+++ internal/storage/elasticsearch.go
@@ -58,7 +58,7 @@ func (e *elasticsearch) Add(oplog *models.Oplog) error {
 
 	routing := resource.Timestamp.Format("02-01-2006")
 
-	meta := []byte(fmt.Sprintf(`{ "index" : { "_index" : "%s", "_type": "_doc", "_routing": "%s" } }%s`, e.source, routing, "\n"))
+	meta := []byte(fmt.Sprintf(`{"index":{"_index":"%s","_type":"_doc","_routing":"%s"}}%s`, e.source, routing, "\n"))
 	data, err := json.Marshal(&resource)
 	if err != nil {
 		return err
